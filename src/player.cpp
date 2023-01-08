c1fcef3939a84ecd271142813a6de24618dfe831
sinku <sininoir@gmail.com>
2023-01-08 12:46:09 +0330

    Add player movement

--- src/player.cpp
+++ src/player.cpp
@@ -2,6 +2,67 @@
 
 using namespace std;
 
-Player::Player() {
+int sign(float number) {
+    if (number > 0) return 1;
+    if (number < 0) return -1;
+    return 0;
+}
 
+Player::Player() {
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(sf::Vector2f(x_position, y_position));
+}
+
+
+void Player::updateWithEvent(sf::Event &event) {
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Up) {
+            y_acceleration = -ACCELERATION_VALUE;
+        } else if (event.key.code == sf::Keyboard::Down) {
+            y_acceleration = ACCELERATION_VALUE;
+        } else if (event.key.code == sf::Keyboard::Right) {
+            x_acceleration = ACCELERATION_VALUE;
+        } else if (event.key.code == sf::Keyboard::Left) {
+            x_acceleration = -ACCELERATION_VALUE;
+        }
+    } else if (event.type == sf::Event::KeyReleased) {
+        if (event.key.code == sf::Keyboard::Up) {
+            y_acceleration = 0;
+        } else if (event.key.code == sf::Keyboard::Down) {
+            y_acceleration = 0;
+        } else if (event.key.code == sf::Keyboard::Right) {
+            x_acceleration = 0;
+        } else if (event.key.code == sf::Keyboard::Left) {
+            x_acceleration = 0;
+        }
+    }
+}
+
+void Player::update() {
+    if (y_acceleration != 0) {
+        // y_velocity = sign(y_velocity) * min(abs(y_velocity + y_acceleration), TOP_SPEED);
+        y_velocity += y_acceleration;
+        if (y_velocity > TOP_SPEED) y_velocity = TOP_SPEED;
+        else if (y_velocity < -TOP_SPEED) y_velocity = -TOP_SPEED;
+    } else {
+        y_velocity = sign(y_velocity) * max(abs(y_velocity) - (ACCELERATION_VALUE / 4), 0.f);
+    }
+
+    if (x_acceleration != 0) {
+        // x_velocity = sign(x_velocity) * min(abs(x_velocity + x_acceleration), TOP_SPEED);
+        x_velocity += x_acceleration;
+        if (x_velocity > TOP_SPEED) x_velocity = TOP_SPEED;
+        else if (x_velocity < -TOP_SPEED) x_velocity = -TOP_SPEED;
+    } else {
+        x_velocity = sign(x_velocity) * max(abs(x_velocity) - (ACCELERATION_VALUE / 4), 0.f);
+    }
+
+    x_position += x_velocity;
+    y_position += y_velocity;
+
+    shape.setPosition(sf::Vector2f(x_position, y_position));
+}
+
+void Player::draw(sf::RenderWindow *window) {
+    window->draw(shape);
 }
--- src/player.hpp
+++ src/player.hpp
@@ -6,13 +6,26 @@
 #include <string>
 
 #define BASE_MAX_HP 100.f
+#define ACCELERATION_VALUE 2.f
+#define TOP_SPEED 14.f
 
 class Player {
     public:
     Player();
 
+    void updateWithEvent(sf::Event &event);
+    void update();
+    void draw(sf::RenderWindow *window);
+
     private:
     float hp = BASE_MAX_HP;
+    float x_velocity = 0.f;
+    float y_velocity = 0.f;
+    float x_acceleration = 0.f;
+    float y_acceleration = 0.f;
+    float x_position = 0.f;
+    float y_position = 0.f;
+    sf::RectangleShape shape = sf::RectangleShape(sf::Vector2f(50,50));
 };
 
 #endif
