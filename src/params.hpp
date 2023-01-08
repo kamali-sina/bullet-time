391393d76d13cac6641950f3d54b8f4c53bb4c5d
sinku <sininoir@gmail.com>
2023-01-08 19:20:17 +0330

    Add new params

--- src/params.hpp
+++ src/params.hpp
@@ -5,8 +5,11 @@
 #define WIN_SIZE_Y 1000
 #define WIN_NAME   "Bullet Time"
 #define BASE_MAX_HP 100.f
-#define ACCELERATION_VALUE 2.f
-#define FRICTION_VALUE     .3f
-#define TOP_SPEED 14.f
+#define ACCELERATION_VALUE 1.8f
+#define FRICTION_VALUE     .4f
+#define TOP_SPEED 14.5f
+#define PLAYER_SIZE_X 50.f
+#define PLAYER_SIZE_Y 50.f
+#define WALL_DAMP_EFFECT 2.f
 
 #endif
