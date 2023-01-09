57bb4104facb897e7e95dd21205c6c3aaa2cffef
sinku <sininoir@gmail.com>
2023-01-09 20:00:59 +0330

    Complete base line class

--- src/line.cpp
+++ src/line.cpp
@@ -1,2 +1,24 @@
 #include "line.hpp"
 
+using namespace std;
+
+float Line::getLenght() {
+    return sqrt(((start.x - end.x) * (start.x - end.x)) + ((start.y - end.y) * (start.y - end.y)));
+}
+
+Line::Line(sf::Vector2f point1, sf::Vector2f point2) {
+    start = point1;
+    end = point2;
+    sf::RectangleShape tempshape(sf::Vector2f(2.f, this->getLenght()));
+    tempshape.setFillColor(sf::Color::Red);
+    tempshape.setPosition(point1);
+    if (end.x - start.x != 0) {
+        float degree = atan((end.y - start.y) / (end.x - start.x)) * (180.0/PI);
+        tempshape.setRotation(degree - 90);
+    }
+    shape = tempshape;
+}
+
+void Line::draw(sf::RenderWindow *window) {
+    window->draw(shape);
+}
--- src/line.hpp
+++ src/line.hpp
@@ -3,10 +3,14 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
+#include "params.hpp"
 
-class line {
+class Line {
 public:
-    line(sf::Vector2f point1, sf::Vector2f point2);
+    Line(sf::Vector2f point1, sf::Vector2f point2);
+    float getLenght();
+    void draw(sf::RenderWindow *window);
 
     sf::Vector2f start;
     sf::Vector2f end;
